887080552699a35492f46ab25c81f9287274b7c8
tester <tester@localhost.localdomain>
2018-02-13 14:02:45 +0800

    new: dev: create git repository tournament.py: functions of sql operations and game matches tournament.sql: table format for database tournament_test.py: test case for tournament

---
+++ tournament.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# 
+# tournament.py -- implementation of a Swiss-system tournament
+#
+
+import psycopg2
+
+
+def connect():
+    """Connect to the PostgreSQL database.  Returns a database connection."""
+    return psycopg2.connect("dbname=tournament")
+
+
+def deleteMatches():
+    """Remove all the match records from the database."""
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("delete from matches")
+    cur.execute("update players set (win,match)=(0,0)")
+    conn.commit()
+    conn.close()
+    return 0
+
+def deletePlayers():
+    """Remove all the player records from the database."""
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("delete from players;")
+    conn.commit()
+    conn.close()
+    return 0
+
+def countPlayers():
+    """Returns the number of players currently registered."""
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("select count(*) from players;")
+    result = cur.fetchall()
+    conn.close()
+    return int(result[0][0])
+
+def registerPlayer(name):
+    """Adds a player to the tournament database.
+  
+    The database assigns a unique serial id number for the player.  (This
+    should be handled by your SQL database schema, not in your Python code.)
+  
+    Args:
+      name: the player's full name (need not be unique).
+    """
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("insert into players (name,win,match) values (%s,%s,%s)", (name,0,0))
+    conn.commit()
+    conn.close()
+    return 0
+
+
+def playerStandings():
+    """Returns a list of the players and their win records, sorted by wins.
+
+    The first entry in the list should be the player in first place, or a player
+    tied for first place if there is currently a tie.
+
+    Returns:
+      A list of tuples, each of which contains (id, name, wins, matches):
+        id: the player's unique id (assigned by the database)
+        name: the player's full name (as registered)
+        wins: the number of matches the player has won
+        matches: the number of matches the player has played
+    """
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("select * from players;")
+    result = cur.fetchall()
+    conn.close()
+    return result
+
+
+def reportMatch(winner, loser):
+    """Records the outcome of a single match between two players.
+
+    Args:
+      winner:  the id number of the player who won
+      loser:  the id number of the player who lost
+    """
+    conn = connect() 
+    cur = conn.cursor()
+    cur.execute("insert into matches (winner,loser) values (%s,%s)",(winner,loser,))
+    cur.execute("update players set win=win+1 where id=(%s)",(winner,))
+    cur.execute("update players set match=match+1 where id=(%s) or id=(%s)",(winner,loser))
+    conn.commit()
+    conn.close()
+    return 0
+ 
+def swissPairings():
+    """Returns a list of pairs of players for the next round of a match.
+  
+    Assuming that there are an even number of players registered, each player
+    appears exactly once in the pairings.  Each player is paired with another
+    player with an equal or nearly-equal win record, that is, a player adjacent
+    to him or her in the standings.
+  
+    Returns:
+      A list of tuples, each of which contains (id1, name1, id2, name2)
+        id1: the first player's unique id
+        name1: the first player's name
+        id2: the second player's unique id
+        name2: the second player's name
+    """
+    result = []
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("select id from players")
+    players = [ element[0] for element in cur.fetchall()]
+    cur.execute("select a.id as id1,a.name as name1,b.id as id2,b.name as name2 from players a, players b where a.id<b.id")
+    matches = cur.fetchall()
+    for match in matches:
+        if match[0] in players and match[2] in players:
+            players.remove(match[0])
+            players.remove(match[2])
+            result.append(match)
+    conn.close()
+    return result
+
